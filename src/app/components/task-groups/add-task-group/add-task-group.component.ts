641c8548a7be512f2adaea5dd5af90df21e51623
johnpapwinter <johnpapahimonas@yahoo.com>
2023-02-01 17:14:49 +0200

    add tf

--- src/app/components/task-groups/add-task-group/add-task-group.component.ts
+++ src/app/components/task-groups/add-task-group/add-task-group.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {TaskForceDto} from "../../../core/domain/dto/task-force.dto";
+import {Router} from "@angular/router";
+import {ApiService} from "../../../core/services/api.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-add-task-group',
@@ -6,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task-group.component.css']
 })
 export class AddTaskGroupComponent {
+  addTaskForceForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+  })
 
+  taskForce: TaskForceDto = { name: '' }
+
+  constructor(private router: Router,
+              private apiService: ApiService,
+              private messageService: MessageService) {
+  }
+
+  addTaskForce() {
+    if (this.addTaskForceForm.value.name !== null) {
+      this.taskForce.name = this.addTaskForceForm.value.name;
+      this.apiService.addTaskGroup(this.taskForce).subscribe(res => {})
+      this.messageService.add({ severity: 'success', summary: 'The task force was successfully added' })
+      this.router.navigateByUrl('home');
+    }
+  }
+
+  onCancel() {
+
+  }
+
+  get addTaskForceFormControl() {
+    return this.addTaskForceForm.controls;
+  }
 }
